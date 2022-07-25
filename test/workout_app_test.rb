b1ebd37b3533eaf41a8a492928587fd6f3e78794
Alex Riviere <alexanderm.riviere@gmail.com>
2022-07-25 16:50:34 +0000

    one more test

--- test/workout_app_test.rb
+++ test/workout_app_test.rb
@@ -42,5 +42,18 @@ class WorkoutTest < Minitest::Test
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "chest"
   end
-
+  
+  def test_add_exercise
+    post '/workouts/new', {workout_name: "chest", workout_date: "2022-07-25"}
+    assert_equal 302, last_response.status
+    
+    workout_id = @storage.find_workout_id("chest")
+    post "/workouts/#{workout_id}/exercises/new", {workout_id: workout_id, exercise_name: "bench" , sets: 4, reps_per_set: 2, weight_lbs: 50, rest_time_mins: 1}
+    assert_equal 302, last_response.status
+    
+    get "/workouts/#{@storage.find_workout_id("chest")}"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Chest"
+    assert_includes last_response.body, "bench"
+  end
 end
